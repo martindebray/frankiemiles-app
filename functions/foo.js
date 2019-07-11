89364adda03ff492a263b0444613d67dff84ae7c
Martin Debray <martin.debray@me.com>
2019-07-11 17:55:16 +0200

    push

--- functions/foo.js
+++ functions/foo.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://frankiemiles.debray.space/wp-json";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then(response => response.json())
     .then(data => ({
       statusCode: 200,
-      body: data.joke
+      body: data
     }))
     .catch(error => ({ statusCode: 422, body: String(error) }));
 };
